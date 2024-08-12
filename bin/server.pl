a88d5c68c0126f7ed49176f59eab0bea5abd196f
ugur.diktas <ugur.diktas@werft22.com>
2024-08-12 17:14:37 +0200

    Working server

--- bin/server.pl
+++ bin/server.pl
@@ -4,15 +4,9 @@ use strict;
 use warnings;
 use HTTP::Server::Simple::CGI;
 use JSON ();
-use CGI;
-use File::Spec;
-use Cwd qw(abs_path getcwd);
-use lib 'lib';
-use Model::DB;
-use Controller::PDFGenerator;
-use Model::Customers;
-use Model::Orders;
-use Model::Items;
+use lib 'lib';  # Add your custom library path
+use Model::DB;  # Database model
+use Controller::PDFGenerator;  # PDF generator controller
 
 {
     package MyWebServer;
@@ -23,56 +17,22 @@ use Model::Items;
         my ($self, $cgi) = @_;
         my $path = $cgi->path_info();
 
-        print "Requested Path: $path\n";  # Debugging output to the terminal
-
-        if ($path eq '/' || $path eq '/index.html') {
-            serve_static_file('public/index.html', 'text/html');
-        } elsif ($path eq '/css/styles.css') {
-            serve_static_file('public/css/styles.css', 'text/css');
-        } elsif ($path eq '/js/app.js') {
-            serve_static_file('public/js/app.js', 'application/javascript');
+        if ($path eq '/api/test') {
+            print "HTTP/1.0 200 OK\r\n";
+            print $cgi->header('text/plain');
+            print "Hello, World!\n";
         } elsif ($path eq '/api/list_orders') {
-            list_orders($cgi);
+            list_orders($cgi);  # Call the function to list orders
         } elsif ($path eq '/api/order_details') {
             my $order_id = $cgi->param('order_id');
-            order_details($cgi, $order_id);
+            order_details($cgi, $order_id);  # Call the function to get order details
         } elsif ($path eq '/api/generate_pdf') {
             my $order_id = $cgi->param('order_id');
-            if ($order_id) {
-                generate_pdf($cgi, $order_id);
-            } else {
-                generate_pdf($cgi);
-            }
-        } elsif ($path eq '/upload') {
-            handle_file_upload($cgi);
+            generate_pdf($cgi, $order_id);  # Call the function to generate PDF
         } else {
             print "HTTP/1.0 404 Not Found\r\n";
-            print $cgi->header('application/json');
-            print JSON::encode_json({ message => "Page not found" });
-        }
-    }
-
-    sub serve_static_file {
-        my ($file_path, $content_type) = @_;
-
-        print "HTTP/1.0 200 OK\r\n";
-
-        my $current_dir = Cwd::getcwd();
-        my $full_path = File::Spec->catfile($current_dir, $file_path);
-        print "Current Directory: $current_dir\n";  # Print current directory
-        print "Attempting to serve file: $full_path\n";  # Debugging output to the terminal
-
-        if (-e $full_path) {
-            open my $fh, '<', $full_path or die "Cannot open file: $!";
-            print "HTTP/1.0 200 OK\r\n";
-            print "Content-Type: $content_type\r\n\r\n";
-            print while <$fh>;
-            close $fh;
-        } else {
-            print "File not found: $full_path\n";  # Debugging output to the terminal
-            print "HTTP/1.0 404 Not Found\r\n";
-            print "Content-Type: text/plain\r\n\r\n";
-            print "File not found";
+            print $cgi->header('text/plain');
+            print "Page not found\n";
         }
     }
 
@@ -136,42 +96,6 @@ use Model::Items;
 
         # Clean up the temporary files
         unlink $pdf_filename;
-    }
-
-    sub handle_file_upload {
-        my ($cgi) = @_;
-
-        my $dbh = Model::DB::connect();
-
-        # Check if a file was uploaded
-        my $upload_filehandle = $cgi->upload('csvfile');
-        if (!$upload_filehandle) {
-            print "HTTP/1.0 400 Bad Request\r\n";
-            print $cgi->header('application/json');
-            print JSON::encode_json({ error => "No file uploaded" });
-            return;
-        }
-
-        # Process the uploaded file
-        my $csv = Text::CSV->new({ binary => 1, auto_diag => 1 });
-        <$upload_filehandle>;  # Skip the header row
-
-        while (my $row = $csv->getline($upload_filehandle)) {
-            my ($order_date, $customer_id, $first_name, $last_name, $order_number, $item_name, $manufacturer, $price) = @$row;
-            
-            # Insert customer data
-            Model::Customers::insert($dbh, $customer_id, $first_name, $last_name);
-            
-            # Insert order data
-            my $order_id = Model::Orders::insert($dbh, $order_number, $order_date, $customer_id);
-            
-            # Insert item data
-            Model::Items::insert($dbh, $item_name, $manufacturer, $price, $order_id);
-        }
-
-        print "HTTP/1.0 200 OK\r\n";
-        print $cgi->header('application/json');
-        print JSON::encode_json({ message => "File uploaded and data processed successfully" });
     }
 }
 
